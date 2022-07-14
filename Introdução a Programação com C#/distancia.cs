d004d9a87c1e834bb893259d3c94b2c1a651b4b3
Gabrielle <76081229+GabrielleKw@users.noreply.github.com>
2022-07-14 14:47:19 -0300

    Create distancia.cs

---
+++ Introdução a Programação com C#/distancia.cs
@@ -0,0 +1,11 @@
+
+using System;
+
+class Desafio {
+  static void Main() {
+    int quilometros = Int32.Parse(Console.ReadLine());
+    int minutos = quilometros * 2;
+    
+    Console.WriteLine(minutos + " minutos");
+  }
+}
